72ff983a6fc9dea6080071a0a3a0550e2589dd24
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-10-29 16:36:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>About Us</h1>
+      <p>At Zappos.com, our purpose is simple: to live and delieverr WOW</p>
+      <p>Twenty years ago, we began as a small online retailer that only sold shoes. Today, We still sell shoes- as well as cloting
+        , handbags, accessories and more.that "more" is providing the very best cutomer service, customer
+        experience, and company culture. We aim to inspire the world by showing it'spossible to simultaneously
+        deliver happiness to customers, employess, vendors, shareholders, and the community in a long-term, sustainble Way.
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      </p>
+      <p>We hope that in the future people won't even realize we started selling shoes online,Instead, they'll know
+        Zappos as a service company that just happens to sell__________.
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a searc
+      </p>
+
+
+    </div>
+
+    
+    
+  );
+}
+
+export default App;
